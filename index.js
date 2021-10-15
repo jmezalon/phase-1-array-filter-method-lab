c5121aa0246873ad7fac89c7b3926b3f69a7ae69
Jean Max Mezalon <+>
2021-10-14 23:05:06 -0400

    commit tree 1d75f5128f6d23e13ec239bb11a320e97e301728

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    const filtered = drivers.filter(el => {
+        let temp = str.slice(0,1).toLowerCase() + str.slice(1, str.length);
+        if (el === temp || el === str) {
+            return el
+        }
+    });
+
+    return filtered ? filtered : []
+}
+
+const fuzzyMatch = (drivers, str) => {
+    let temp = str.slice(0,1).toLowerCase() + str.slice(1, str.length)
+  let tempLength = temp.length;
+
+  const matchDrivers = drivers.filter(driver => {
+    let name = driver.slice(0, tempLength)
+    if (name === temp || name === str) {
+      return name
+    }
+  })
+  
+  return matchDrivers
+}
+
+function matchName(drivers, str) {
+    return drivers.filter(el => el.name === str)
+  }
